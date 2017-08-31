2cbee80e44c33bb89068e939a93eb2de200b89d5
Manuel Izurieta <manuel@aaa.aaa>
2017-08-31 00:28:15 -0300

    first commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other. transform.rotation);
+		}
+
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+
+}
+
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+	private Rigidbody rb;
+	private AudioSource audioSource ;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource >();
+	}
+
+	void Update()
+	{
+		//if (Input.GetButton("Fire1") && Time.time > nextFire) //MIK
+		if( CrossPlatformInputManager.GetButton("Fire") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audioSource.Play();
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = 0.0f;
+		float moveVertical = 0.0f;
+
+		#if UNITY_IOS
+			moveHorizontal = CrossPlatformInputManager.GetAxis ("Horizontal");	
+			moveVertical = CrossPlatformInputManager.GetAxis ("Vertical");	
+		#endif
+
+		#if UNITY_EDITOR
+			moveHorizontal = Input.GetAxis ("Horizontal");	
+			moveVertical = Input.GetAxis ("Vertical");	
+		#endif
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+
+	}
+}
